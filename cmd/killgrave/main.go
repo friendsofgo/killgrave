a6bd838b061e6aa53336187a73f2c1ee5258972e
Adrián Pérez <perezg.adrian@gmail.com>
2019-04-13 12:32:40 +0200

    Run basic server

--- cmd/killgrave/main.go
+++ cmd/killgrave/main.go
@@ -1,5 +1,22 @@
 package main
 
-func main() {
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
 
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	host := flag.String("host", "localhost", "if you run your server on a different host (default: localhost)")
+	port := flag.Int("port", 3000, "por to run the server (default: 3000)")
+	flag.Parse()
+
+	r := mux.NewRouter()
+
+	httpAddr := fmt.Sprintf(":%d", *port)
+	log.Printf("The fake server is on tap now: http://%s:%d\n", *host, *port)
+	log.Fatal(http.ListenAndServe(httpAddr, r))
 }
--- go.mod
+++ go.mod
@@ -1,1 +1,5 @@
 module github.com/friendsofgo/killgrave
+
+go 1.12
+
+require github.com/gorilla/mux v1.7.1
