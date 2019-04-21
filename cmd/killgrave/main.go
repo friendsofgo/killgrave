5fb087f837f6cee2c7a1d33e0861c0f9d012abdf
aperezg <perezg.adrian@gmail.com>
2019-04-21 11:00:55 +0200

    Add version option

--- cmd/killgrave/main.go
+++ cmd/killgrave/main.go
@@ -11,11 +11,22 @@ import (
 	"github.com/gorilla/mux"
 )
 
+var (
+	version = "dev"
+	name    = "Killgrave"
+)
+
 func main() {
 	host := flag.String("host", "localhost", "if you run your server on a different host")
 	port := flag.Int("port", 3000, "por to run the server")
 	imposters := flag.String("imposters", "imposters", "directory where your imposter are saved")
+	v := flag.Bool("version", false, "show the version of the application")
 	flag.Parse()
+
+	if *v {
+		fmt.Printf("%s version %s\n", name, version)
+		return
+	}
 
 	r := mux.NewRouter()
 
