e2440eca53d42510d74187568a795af5adc57bb5
aperezg <perezg.adrian@gmail.com>
2020-01-03 11:20:13 +0100

    add https compatibility

--- internal/server/http/proxy.go
+++ internal/server/http/proxy.go
@@ -12,6 +12,7 @@ import (
 type Proxy struct {
 	server *httputil.ReverseProxy
 	mode   killgrave.ProxyMode
+	url    *url.URL
 }
 
 // NewProxy creates new proxy server.
@@ -21,12 +22,17 @@ func NewProxy(rawurl string, mode killgrave.ProxyMode) (*Proxy, error) {
 		return nil, err
 	}
 	reverseProxy := httputil.NewSingleHostReverseProxy(u)
-	return &Proxy{reverseProxy, mode}, nil
+	return &Proxy{server: reverseProxy, mode: mode, url: u}, nil
 }
 
 // Handler returns handler that sends request to another server.
 func (p *Proxy) Handler() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
+		r.URL.Host = p.url.Host
+		r.URL.Scheme = p.url.Scheme
+		r.Header.Set("X-Forwarded-Host", r.Header.Get("Host"))
+		r.Host = p.url.Host
+
 		p.server.ServeHTTP(w, r)
 	}
 }
