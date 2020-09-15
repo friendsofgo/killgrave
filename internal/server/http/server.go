f0bc03ffd4c9214ed40296aa0e6544b2816203a7
Mario Arranz <mario@paddle.com>
2020-09-15 12:43:20 +0200

    remove cert files and use them as const

--- internal/server/http/server.go
+++ internal/server/http/server.go
@@ -2,6 +2,7 @@ package http
 
 import (
 	"context"
+	"crypto/tls"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
@@ -14,6 +15,40 @@ import (
 	"github.com/gorilla/mux"
 
 	killgrave "github.com/friendsofgo/killgrave/internal"
+)
+
+const (
+	serverCert = `-----BEGIN CERTIFICATE-----
+MIIBuzCCASQCCQD338luSXvgxjANBgkqhkiG9w0BAQsFADAiMQswCQYDVQQGEwJH
+QjETMBEGA1UECgwKUGFkZGxlLmNvbTAeFw0yMDA5MTQwNjQyMjhaFw0yMTA5MTQw
+NjQyMjhaMCIxCzAJBgNVBAYTAkdCMRMwEQYDVQQKDApQYWRkbGUuY29tMIGfMA0G
+CSqGSIb3DQEBAQUAA4GNADCBiQKBgQCpleDDAedGni6+4fD3A9Lmd80jaovtr8Bw
+U1Fo189bOjzUlfHMK63EEMU7OWzlaVVhM6Vs4528/auIvwd7rCx9cz5LXRzjktWR
+GjCkxnVFSOvf+OoDxQjKl2CC5xExwwanB/flXr91DKEvLB/0/xjMBy85N6KtEpp9
+upbI5On60wIDAQABMA0GCSqGSIb3DQEBCwUAA4GBADeFvwh0TYj/j8qTuTLaKtmH
+ijxR8ycUXlQwEMxI1sa2TlQwDDdJgafl1cKNk/Kckhh4kFxXoLoXpOhaq2aljjSg
+6YuHsMOIUt0tRlBF6TjvPPcBDa66SBiFxLfdXgH16D2Im5Zp8BLV3gF72ZUSGFQ1
+umGPWNu/ak60W7Gt1dom
+-----END CERTIFICATE-----
+`
+
+	serverKey = `-----BEGIN PRIVATE KEY-----
+MIICdwIBADANBgkqhkiG9w0BAQEFAASCAmEwggJdAgEAAoGBAKmV4MMB50aeLr7h
+8PcD0uZ3zSNqi+2vwHBTUWjXz1s6PNSV8cwrrcQQxTs5bOVpVWEzpWzjnbz9q4i/
+B3usLH1zPktdHOOS1ZEaMKTGdUVI69/46gPFCMqXYILnETHDBqcH9+Vev3UMoS8s
+H/T/GMwHLzk3oq0Smn26lsjk6frTAgMBAAECgYBueJgF8IN8WScSaZTTFMaaI4ia
+fZnRqB+wDFaH+a22O+XMhjERU5LKmjIhNnNdrpRT0ZT9rfjfWFAZsHdrdOk1b6pD
+C3jIb7A/fRINxzLIiaf3NjzQAPjEUDRdIramuN649++w/ReorRRYXghx9wQttsYs
+R6RLgS9lSR0LvuWygQJBANyM6bSjmVfcUjLggxks5Kf/SEWtdVUzQbjxd7XTBqg+
+DcnrSwhbtEAsdrdMyM2K2vH/y2Pw5TCBEon9lXgD9+MCQQDE1+LoLlXDwqKUmdKc
+wM/AgqqQUCnOTzTps/iDUsma+pLrDfbq9BzgJGIwdep/83gUtMul+WImldFqAaE1
+YwRRAkEAhErmGJuVbJVUnP38xUofcgH3yQvN+KhBQP1ecCs/sAVg4gCIRmAzk9bX
+8wVU4a3rvQNRbgZeg6S4iRsZ4B6JPwJAGYJUsHwYtzbI9PL+PTwrcCo+ylbpJAhV
+theRlAstfz0LvxRfNFGQUGfGpbsSOaSmoiBP5+Fqm898hP1TE+USEQJBAJcysWBX
+RMpOrFy9SdSAjdBIYFblDO+JLdinIe74NvHgEiFtm/Lq39poGpTfCmhIS+e0ENi3
+EccAIHrj9VmeAwA=
+-----END PRIVATE KEY-----
+`
 )
 
 var (
@@ -127,10 +162,20 @@ func (s *Server) Run() {
 }
 
 func (s *Server) run(secure bool) error {
-	if secure {
-		return s.httpServer.ListenAndServeTLS("server.cert", "server.key")
+	if !secure {
+		return s.httpServer.ListenAndServe()
 	}
-	return s.httpServer.ListenAndServe()
+
+	cert, err := tls.X509KeyPair([]byte(serverCert), []byte(serverKey))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	s.httpServer.TLSConfig = &tls.Config{
+		Certificates: []tls.Certificate{cert},
+	}
+
+	return s.httpServer.ListenAndServeTLS("", "")
 }
 
 // Shutdown shutdown the current http server
